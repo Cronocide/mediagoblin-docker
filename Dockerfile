5b568447806546b34fa987e573ba965f102ee6d8
Daniel Dayley <ddayley@qualtrics.com>
2023-08-20 14:41:47 -0600

    Early work

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,55 @@
 FROM debian
 ENV PROJ_NAME=mediagoblin-docker
+ARG DEBIAN_FRONTEND=noninteractive
+
+# Install dependencies
+RUN apt update && apt install -y \
+	automake \
+	git \
+	nodejs \
+	npm \
+	python3-dev \
+	python3-gst-1.0 \
+	python3-psycopg2 \
+	python3-lxml \
+	python3-pil \
+	python3-setuptools \
+	virtualenv \
+	nginx \
+	rabbitmq-server
+# Setup user
+ARG USER_UID=999
+ARG USER_NAME=mediagoblin
+ARG BUILD_BRANCH=stable
+RUN useradd \
+	--system \
+	-u $USER_UID \
+	--create-home \
+	--home-dir \
+	/var/lib/mediagoblin \
+	--group www-data \
+	--comment 'GNU MediaGoblin system account' \
+	$USER_NAME
+RUN usermod --append --groups $USER_NAME $USER_NAME
+RUN mkdir --parents /srv/mediagoblin
+RUN chown --no-dereference --recursive $USER_NAME:www-data /srv/mediagoblin
+RUN chown --recursive $USER_NAME:www-data /etc/nginx
+USER $USER_NAME
+RUN git clone --depth=1 https://git.savannah.gnu.org/git/mediagoblin.git \
+	--branch $BUILD_BRANCH \
+	--recursive \
+	/srv/mediagoblin/mediagoblin
+WORKDIR /srv/mediagoblin/mediagoblin
+RUN ./bootstrap.sh
+RUN ./configure
+# Resolve setuptools AttributeError for SQLAlchemy install
+ENV SETUPTOOLS_USE_DISTUTILS=stdlib
+RUN make
 
 # Copy project files
 ADD ./entrypoint.sh /entrypoint.sh
+ADD ./nginx.conf /etc/nginx/conf.d/
 
 # Run entrypoint
 ENTRYPOINT ["/entrypoint.sh"]
+
---
+++ entrypoint.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+# Mediagoblin launcher
+# v1.0 August 2023 by Cronocide
+
+# Global constants
+PORT_REGEX='^[0-9]{1,5}$'
+
+__set_defaults() {
+	[ -z "$BIND_PORT" ] && export BIND_PORT=8080
+#	[ -z "$POSTGRES_DB" ] && export POSTGRES_DB=mediagoblin
+#	[ -z "$POSTGRES_USER" ] && export POSTGRES_USER=mediagoblin
+#	[ -z "$POSTGRES_HOST" ] && export POSTGRES_HOST=127.0.0.1
+#	[ -z "$POSTGRES_PORT" ] && export POSTGRES_HOST=5432
+	# Configured defaults from deploy guide
+	export CELERY_CONFIG_MODULE=mediagoblin.init.celery.from_celery
+}
+
+# Verify environment variables
+__verify_env() {
+	# Check for required files
+	! [ -e /srv/mediagoblin/mediagoblin/mediagoblin.ini ] && echo "Missing mediagoblin.ini, aborting." && exit 1
+	# Check for required vars
+	REQUIRED_ENV_VARS="BIND_PORT SERVER_NAME ADMIN_USERNAME ADMIN_EMAIL ADMIN_PASSWORD"
+	for REQUIRED_VAR in $(echo "$REQUIRED_ENV_VARS"); do
+		[ -z "${!REQUIRED_VAR}" ] && echo "Missing required ENV variable $REQUIRED_VAR, aborting." && exit 1
+	done
+	# Check for rational ports
+#	PORT_VARS="POSTGRES_PORT"
+#	for PORT_VAR in $(echo "$PORT_VARS"); do
+#	[[ $(echo "${!PORT_VAR}" | egrep "$PORT_REGEX") == "" ]] && \
+#		echo "Unable to parse encryption port ${!PORT_VAR}, should be 1-65535" && exit 1
+#	done
+	# Populate Nginx config
+	for TEMPLATE_VAR in $(echo "BIND_PORT SERVER_NAME PROXY_PREFIX"); do
+			sed -i "s%\$$TEMPLATE_VAR%${!TEMPLATE_VAR}%" /etc/nginx/conf.d/nginx.conf
+	done
+}
+
+# Create admin user
+__create_user() {
+	/srv/mediagoblin/mediagoblin/bin/gmg adduser --username "$ADMIN_USERNAME" --email "$ADMIN_EMAIL"
+}
+
+# Main run loop
+__run_main() {
+	# Link user (media) directories
+	ln /srv/mediagoblin/mediagoblin/user_dev/ /data/
+	# Run nginx
+	nginx &
+	# Run MediaGoblin
+	/srv/mediagoblin/mediagoblin/lazyserver.sh &
+	# Run Celery
+	/srv/mediagoblin/mediagoblin/lazycelery.sh &
+	# Create user
+	sleep 3
+	__create_user
+}
+
+# Optional variables
+# POSTGRES_HOST
+# POSTGRES_PORT
+# RABBITMQ_HOST
+# RABBITMQ_PORT
+# RABBITMQ_USER
+# RABBITMQ_PASS
+
+# Main functions
+
+__set_defaults && __verify_env
+__run_main
